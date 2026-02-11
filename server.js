b8530a95928ad2526c28ddebf9f82c51787d6dff
juhibhagat <jkri0206@gmail.com>
2026-02-11 22:31:07 +0530

    mongodb path that  store in variable

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongodbClient = require("mongodb").MongoClient;
-mongodbClient.connect("mongodb://127.0.0.1:27017")
+const mongodbConnection= "mongodb://127.0.0.1:27017"
+mongodbClient.connect(mongodbConnection)
 .then(()=>{
     console.log("Connect with mongoDb Server")
 }).catch( error=>{
