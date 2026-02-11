0a8a49b34ca64a99567f4a9a03048eb6e21dc761
juhibhagat <jkri0206@gmail.com>
2026-02-11 23:11:47 +0530

    i make mongodb driver configure

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const mongodbClient = require("mongodb").MongoClient;
-const mongodbConnection= "mongodb://127.0.0.1:27017"
-mongodbClient.connect(mongodbConnection)
-.then(()=>{
-    console.log("Connect with mongoDb Server")
-}).catch( error=>{
-    console.log(error)
-})
+const url = "mongodb://127.0.0.1:27017"
+const client = new mongodbClient(url);
+ const dbname = 'Zomato';
+
+async function Main() {
+   await client.connect();
+   
+    console.log("databaseconnect ")
+    const db = client.db(dbname)
+    const collection = await db.collection('users').find({id:10}).toArray();
+    console.log(collection);
+}
+Main()
 
